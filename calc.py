56b718217bc7b7cd8b9bf84eb7d427074941d8c5
Geeta95 <32121842+Geeta95@users.noreply.github.com>
2019-06-20 06:35:01 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,23 @@
+def add(num1,num2):
+  return num1+num2
+def sub(num1,num2):
+  return num1-num2
+def mul(num1,num2):
+  return num1*num2
+def div(num1,num2):
+  return num1/num2
+  
+  
+print("select any operation :\n""1.Addition \n""2.Subtraction \n""3.Multiplication \n""4.Division \n")
+select =int(input("select from 1,2,3,4:")
+num1 = int(input("enter number1 :")
+num2 = int(input("enter number2 :")
+if select == '1' :
+  print(num1,"+",num2 is add(num1)(num2))
+elif select == '2'
+  print(num1,"-",num2 is sub(num1)(num2))
+elif select == '3'
+  print(num1,"*",num2 mul(num1)(num2))
+elif select == '4'
+  print(num1,"/",num2 div(num1)(num2))
+  
